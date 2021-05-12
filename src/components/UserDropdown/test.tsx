06ddd14173792646b5332982aeb77a9b3cbc573f
Jefferson Felipe <felipej2626@gmail.com>
2021-05-12 19:51:42 -0300

    Criação do componente de UserDropdown

---
+++ src/components/UserDropdown/test.tsx
@@ -0,0 +1,27 @@
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import UserDropdown from '.'
+
+describe('<UserDropdown />', () => {
+  it('should render the username', () => {
+    renderWithTheme(<UserDropdown username="Jeff" />)
+
+    expect(screen.getByText(/jeff/i)).toBeInTheDocument()
+  })
+
+  it('should render the menu', () => {
+    renderWithTheme(<UserDropdown username="Jeff" />)
+
+    // open menu
+    userEvent.click(screen.getByText(/jeff/i))
+
+    expect(
+      screen.getByRole('link', { name: /my profile/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /wishlist/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
+  })
+})
