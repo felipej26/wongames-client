da560b1cdd41c950accb9dfe8844c30a4f73ab9e
Jefferson Felipe <felipej2626@gmail.com>
2021-05-12 21:47:10 -0300

    Alteração para permitir passar o Slug para os GameCards

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,7 +1,9 @@
-import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
-import filterItemsMock from 'components/ExploreSidebar/mock'
 import { initializeApollo } from 'utils/apollo'
 import { QUERY_GAMES } from 'graphql/queries/games'
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
+
+import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
+import filterItemsMock from 'components/ExploreSidebar/mock'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
@@ -10,7 +12,7 @@ export default function GamesPage(props: GamesTemplateProps) {
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
 
-  const { data } = await apolloClient.query({
+  const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
     query: QUERY_GAMES,
     variables: {
       limit: 9
@@ -21,6 +23,7 @@ export async function getStaticProps() {
     props: {
       revalidate: 60,
       games: data.games.map((game) => ({
+        slug: game.slug,
         title: game.name,
         developer: game.developers[0].name,
         img: `http://localhost:1337${game.cover?.url ?? ''}`,
