57b0f62f314538499a20303118658a7c3b84b8cd
Jefferson Felipe <felipej2626@gmail.com>
2021-05-19 22:35:29 -0300

    Conserto da revalidação dos dados no Next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,13 +18,13 @@ export async function getStaticProps() {
     data: { banners, newGames, upcomingGames, freeGames, sections }
   } = await apolloClient.query<QueryHome, QueryHomeVariables>({
     query: QUERY_HOME,
-    variables: { date: TODAY }
+    variables: { date: TODAY },
+    fetchPolicy: 'no-cache'
   })
 
   return {
+    revalidate: 10,
     props: {
-      revalidate: 60,
-
       banners: bannerMapper(banners),
 
       newGamesTitle: sections?.newGames?.title,
