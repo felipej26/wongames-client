2381fcd4f23b6b5b70377a6404eaa6e72571d40a
Jefferson Felipe <felipej2626@gmail.com>
2021-05-24 18:43:57 -0300

    Ajuste para o usuario continuar na pagina que ele estava tentando acessar antes do Login

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -16,7 +16,8 @@ const FormSignIn = () => {
   const [fieldError, setFieldError] = useState<FieldErrors>({})
   const [values, setValues] = useState({ email: '', password: '' })
   const [loading, setLoading] = useState(false)
-  const { push } = useRouter()
+  const routes = useRouter()
+  const { push, query } = routes
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
@@ -25,7 +26,7 @@ const FormSignIn = () => {
     const result = await signIn('credentials', {
       ...values,
       redirect: false,
-      callbackUrl: '/'
+      callbackUrl: `${window.location.origin}${query?.callbackUrl || ''}`
     })
 
     const errors = signInValidate(values)
