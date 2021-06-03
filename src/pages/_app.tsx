b2c1d892f45888d33177ea666897edac3cd2637e
Jefferson Felipe <felipej2626@gmail.com>
2021-06-03 17:28:48 -0300

    Criação do hook useWishlist

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Provider as AuthProvider } from 'next-auth/client'
 import { ApolloProvider } from '@apollo/client'
 import { ThemeProvider } from 'styled-components'
 import { CartProvider } from 'hooks/use-cart'
+import { WishlistProvider } from 'hooks/use-wishlist'
 
 import { AppProps } from 'next/app'
 import Head from 'next/head'
@@ -20,25 +21,27 @@ function App({ Component, pageProps }: AppProps) {
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
           <CartProvider>
-            <Head>
-              <title>Won Games</title>
-              <link rel="shortcut icon" href="/img/icon-512.png" />
-              <link rel="apple-touch-icon" href="/img/icon-512.png" />
-              <link rel="manifest" href="/manifest.json" />
-              <meta name="theme-color" content="#06092B" />
-              <meta
-                name="description"
-                content="The best Game Stores in the world!"
+            <WishlistProvider>
+              <Head>
+                <title>Won Games</title>
+                <link rel="shortcut icon" href="/img/icon-512.png" />
+                <link rel="apple-touch-icon" href="/img/icon-512.png" />
+                <link rel="manifest" href="/manifest.json" />
+                <meta name="theme-color" content="#06092B" />
+                <meta
+                  name="description"
+                  content="The best Game Stores in the world!"
+                />
+              </Head>
+              <GlobalStyles />
+              <NextNProgress
+                color={theme.colors.primary}
+                startPosition={0.3}
+                stopDelayMs={200}
+                height={5}
               />
-            </Head>
-            <GlobalStyles />
-            <NextNProgress
-              color={theme.colors.primary}
-              startPosition={0.3}
-              stopDelayMs={200}
-              height={5}
-            />
-            <Component {...pageProps} />
+              <Component {...pageProps} />
+            </WishlistProvider>
           </CartProvider>
         </ThemeProvider>
       </ApolloProvider>
