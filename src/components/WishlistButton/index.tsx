a4d6778b61dd2500a36a35916d42ad681ad06cfc
Jefferson Felipe <felipej2626@gmail.com>
2021-10-07 21:23:51 -0300

    Add Spinner to WishlistButton

--- src/components/WishlistButton/index.tsx
+++ src/components/WishlistButton/index.tsx
@@ -1,8 +1,10 @@
 import Button, { ButtonProps } from 'components/Button'
+import { useState } from 'react'
 import { useWishlist } from 'hooks/use-wishlist'
 import { useSession } from 'next-auth/client'
 
 import { Favorite, FavoriteBorder } from 'styled-icons/material-outlined'
+import Spinner from 'components/Spinner'
 
 type WishlistButtonProps = {
   id: string
@@ -15,10 +17,13 @@ const WishlistButton = ({
   size = 'small'
 }: WishlistButtonProps) => {
   const [session] = useSession()
+  const [loading, setLoading] = useState(false)
   const { isInWishlist, addToWishlist, removeFromWishlist } = useWishlist()
 
-  const handleClick = () => {
-    isInWishlist(id) ? removeFromWishlist(id) : addToWishlist(id)
+  const handleClick = async () => {
+    setLoading(true)
+    isInWishlist(id) ? await removeFromWishlist(id) : await addToWishlist(id)
+    setLoading(false)
   }
 
   const ButtonText = isInWishlist(id)
@@ -30,7 +35,9 @@ const WishlistButton = ({
   return (
     <Button
       icon={
-        isInWishlist(id) ? (
+        loading ? (
+          <Spinner />
+        ) : isInWishlist(id) ? (
           <Favorite aria-label={ButtonText} />
         ) : (
           <FavoriteBorder aria-label={ButtonText} />
