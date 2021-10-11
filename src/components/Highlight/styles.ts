5df6a07042c6aba7dcd344cec006ea4d77cb16d7
Jefferson Felipe <felipej2626@gmail.com>
2021-10-11 14:34:20 -0300

    Update Highlight to use next/image

--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 import { HighlightProps } from '.'
 
-type WrapperProps = Pick<HighlightProps, 'backgroundImage' | 'alignment'>
+type WrapperProps = Pick<HighlightProps, 'alignment'>
 
 const wrapperModifiers = {
   right: () => css`
@@ -22,20 +22,17 @@ const wrapperModifiers = {
       text-align: left;
     }
 
-    ${FloatImage} {
+    ${FloatImageWrapper} {
       justify-self: end;
     }
   `
 }
 
 export const Wrapper = styled.section<WrapperProps>`
-  ${({ backgroundImage, alignment }) => css`
-    position: relative;
-    background-image: url(${backgroundImage});
-    background-position: center center;
-    background-size: cover;
-    height: 23rem;
+  ${({ alignment }) => css`
     display: grid;
+    height: 23rem;
+    position: relative;
 
     &::after {
       content: '';
@@ -43,6 +40,11 @@ export const Wrapper = styled.section<WrapperProps>`
       width: 100%;
       height: 100%;
       background-color: rgba(0, 0, 0, 0.6);
+    }
+
+    img {
+      position: absolute;
+      object-fit: cover;
     }
 
     ${media.greaterThan('medium')`
@@ -53,13 +55,18 @@ export const Wrapper = styled.section<WrapperProps>`
   `}
 `
 
-export const FloatImage = styled.img`
+export const FloatImageWrapper = styled.div`
   ${({ theme }) => css`
     grid-area: floatimage;
     z-index: ${theme.layers.base};
     max-height: 23rem;
     max-width: 100%;
     align-self: end;
+
+    img {
+      position: relative;
+      object-fit: contain;
+    }
 
     ${media.greaterThan('medium')`
       height: 32rem;
